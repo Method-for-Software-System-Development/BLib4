92a0d58c65bacce2f10ed4f3ea81ea8aee7873c2
Tal Yagudin <37711526+tal1569@users.noreply.github.com>
2025-01-04 13:15:21 +0200

    update client book entitie

--- BLib4/ClientSide/src/entities/book/Book.java
+++ BLib4/ClientSide/src/entities/book/Book.java
@@ -1,80 +1,142 @@
 package entities.book;
 
-import java.util.HashMap;
+import javafx.scene.image.Image;
+
 import java.util.Date;
+import java.util.HashMap;
 
-public class Book {
-	private final int book_id;
-	private final String title;
-	private final int edition_number;
-	private final Date printDate;
-	private final String subject;
-	private final String description;
-	private HashMap<String,String> waitlist = new HashMap<>();
-	
-	/**
-	 * Constructor to initialize the Book
-	 *
-	 * @param book_id       	The unique identifier for the book
-	 * @param title         	The title of the book
-	 * @param edition_number    The edition number of the book
-	 * @param printDate     	The date the book was printed
-	 * @param subject       	The subject or genre of the book
-	 * @param description   	A brief description of the book
-	 */
-	public Book(int book_id, String title, int edition_number, Date printDate, String subject, String description) {
-		this.book_id = book_id;
-		this.title = title;
-		this.edition_number = edition_number;
-		this.printDate = printDate;
-		this.subject = subject;
-		this.description = description;
-	}
+public class Book
+{
+    private final int book_id;
+    private final String title;
+    private final int edition_number;
+    private final Date printDate;
+    private final String subject;
+    private final String description;
+    private Image image;
+    private final HashMap<String, Date> waitlist = new HashMap<>();
 
-	public int getBookid() {
-		return book_id;
-	}
+    /**
+     * Constructor to initialize the Book
+     *
+     * @param book_id        The unique identifier for the book
+     * @param title          The title of the book
+     * @param edition_number The edition number of the book
+     * @param printDate      The date the book was printed
+     * @param subject        The subject or genre of the book
+     * @param description    A brief description of the book
+     */
+    public Book(int book_id, String title, int edition_number, Date printDate, String subject, String description)
+    {
+        this.book_id = book_id;
+        this.title = title;
+        this.edition_number = edition_number;
+        this.printDate = printDate;
+        this.subject = subject;
+        this.description = description;
+    }
 
-	public String getTitle() {
-		return title;
-	}
+    /**
+     * Getter to the book id
+     * @return - The book id
+     */
+    public int getBookId()
+    {
+        return book_id;
+    }
 
-	public int getEditionNum() {
-		return edition_number;
-	}
+    /**
+     * Getter to the book title
+     * @return - The book title
+     */
+    public String getTitle()
+    {
+        return title;
+    }
 
-	public Date getPrintDate() {
-		return printDate;
-	}
+    /**
+     * Getter to the book edition number
+     * @return - The book edition number
+     */
+    public int getEditionNum()
+    {
+        return edition_number;
+    }
 
-	public String getSubject() {
-		return subject;
-	}
+    /**
+     * Getter to the book print date
+     * @return - The book print date
+     */
+    public Date getPrintDate()
+    {
+        return printDate;
+    }
 
-	public String getDescription() {
-		return description;
-	}
+    /**
+     * Getter to the book subject
+     * @return - The book subject
+     */
+    public String getSubject()
+    {
+        return subject;
+    }
 
-	//Getter to all the wait list of the book
-	public HashMap<String, String> getWaitlist() {
-		return waitlist;
-	}
-	
-	//Adding an order to the book wait list
-	public void addSubscriberToWaitList(String SubscriberID, String orderDateTime) {
-		//Adding a subscriber order to the wait list only if he didn't ordered the book already
-		if(!waitlist.containsKey(SubscriberID)) {
-			waitlist.put(SubscriberID, orderDateTime);
-		}
-	}
-	
-	//Removing an order from book wait list
-	public void removeOrder(String SubscriberID) {
-		//Checking if the wait list contains the subscriber id as a key in it in order to delete the order
-		if(waitlist.containsKey(SubscriberID)) { 
-			waitlist.remove(SubscriberID);
-		}
-	}
-	
-	
+    /**
+     * Getter to the book description
+     * @return - The book description
+     */
+    public String getDescription()
+    {
+        return description;
+    }
+
+    /**
+     * Getter to the book image
+     * @return - The book image
+     */
+    public Image getImage()
+    {
+        return image;
+    }
+
+    /**
+     * Get the book wait list
+     * @return - The book wait list as a HashMap
+     */
+    public HashMap<String, Date> getWaitlist()
+    {
+        return waitlist;
+    }
+
+    /**
+     * Set the book image
+     * @param image - The book image
+     */
+    public void setImage(Image image)
+    {
+        this.image = image;
+    }
+
+    /**
+     * Add a subscriber to the book wait list
+     * @param SubscriberID - The subscriber id
+     * @param orderDateTime - The date and time the subscriber ordered the book
+     */
+    public void addSubscriberToWaitlist(String SubscriberID, Date orderDateTime)
+    {
+        //Adding a subscriber order to the wait list only if he didn't order the book already
+        if (!waitlist.containsKey(SubscriberID))
+        {
+            waitlist.put(SubscriberID, orderDateTime);
+        }
+    }
+
+    /**
+     * Remove a subscriber from the book wait list
+     * @param SubscriberID - The subscriber id
+     */
+    public void removeSubscriberFromWaitlist(String SubscriberID)
+    {
+        waitlist.remove(SubscriberID);
+    }
 }
