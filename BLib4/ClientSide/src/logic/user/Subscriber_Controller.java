dc8b5cfd801af3c23a940e3dce31ba6d7e59f725
noas1712 <noas1712@gmail.com>
2025-01-10 12:14:18 +0200

    Changes of methods signature in Subscriber_controller and deleting testing methods

--- BLib4/ClientSide/src/logic/user/Subscriber_Controller.java
+++ BLib4/ClientSide/src/logic/user/Subscriber_Controller.java
@@ -1,69 +1,26 @@
 package logic.user;
 
-import java.io.IOException;
 import java.util.ArrayList;
-
 import entities.logic.MessageType;
 import entities.user.Librarian;
 import entities.user.Subscriber;
-import gui.common.MainWindowFrameController;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
-import javafx.stage.Stage;
 import logic.communication.ChatClient;
 import logic.communication.ClientUI;
 
-public class Subscriber_Controller {
-	@FXML
-    private TextField txtID;
+public class Subscriber_Controller {   
 
-    @FXML
-    private TextField txtFirstName;
-
-    @FXML
-    private TextField txtLastName;
-
-    @FXML
-    private TextField txtPhone;
-
-    @FXML
-    private TextField txtEmail;
-
-    @FXML
-    private TextField txtPassword;
-    
-    @FXML
-    private TextField txtType;
-    
-    @FXML
-    private Button btnSignUp;
-    
-    @FXML
-    private Button btnLogIn;
-    
-    private Stage currentStage;
-
-    //handler to read new subscriber data and call addNewSubscriber
-    @FXML
-    private void handleSignUp(ActionEvent event) {
-    	String userID = txtID.getText();
-        String firstName = txtFirstName.getText();
-        String lastName = txtLastName.getText();
-        String phoneNumber = txtPhone.getText();
-        String email = txtEmail.getText();
-        String password = txtPassword.getText();
-        addNewSubscriber(userID,firstName,lastName,phoneNumber,email,password);
-    }
-    
-    //sending new subscriber data to server
-	private void addNewSubscriber(String userID, String firstName, String lastName, String phoneNumber, String email,String password) {
+	/**
+     * This method send a request to the server to sign up a new subscriber.
+     *
+     * @param userID ID of the user.
+     * @param firstName	first name of the user.
+     * @param lastName	last name of the user.
+     * @param phoneNumber phone number of the user.
+     * @param email email of the user.
+     * @param password	password of the user.
+     */
+	public void addNewSubscriber(String userID, String firstName, String lastName, String phoneNumber, String email,String password) {
         // Create a list of data of the new subscriber to send to the server
         ArrayList<String> dataOfNewSubscriber = new ArrayList<>();	
         dataOfNewSubscriber.add(userID);
@@ -86,60 +43,50 @@ public class Subscriber_Controller {
             Alert alert = new Alert(Alert.AlertType.ERROR, "Failed to sign up the subscriber");
             alert.setHeaderText("Error");
             alert.showAndWait();
-        }
-        
+        }   
 	}
-	
-	 //handler to log in subscriber or librarian
-    @FXML
-    private void handleLogIn(ActionEvent event) throws IOException {
-        currentStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-    	String userID = txtID.getText();
-        String password = txtPassword.getText();
-        String type = txtType.getText();
-//        attemptLogin(userID,password,type);
-    }
 
-//    public Subscriber attemptLoginAsSubscriber(String userID, String password)
-//    {
-//        //if found return the subscriber, else return null. try use try-catch block to handle the IO exception from the server
-//    }
-//
-//    public Librarian attemptLoginAsLibrarian(String userID, String password)
-//    {
-//        //if found return the librarian, else return null. try use try-catch block to handle the IO exception from the server
-//    }
-//
-//    // DELETE:
-//    private void attemptLogin(String userID, String password, String type) throws IOException {
-//    	// Create a list of data of the new subscriber to send to the server
-//        ArrayList<String> dataOfLogIn = new ArrayList<>();
-//        dataOfLogIn.add(userID);
-//        dataOfLogIn.add(password);
-//        dataOfLogIn.add(type);
-//        ClientUI.chat.accept(new MessageType("100",dataOfLogIn));
-//        //check user verification from db by type
-//        if(ChatClient.serverResponse) {
-//        	switch (type)
-//        	{
-//        	case "subscriber":
-//        		logInSubscriber(ChatClient.subscribers.get(0));
-//        		break;
-//
-//        	case "librarian":
-//        		logInLibrarian(ChatClient.librarian);
-//        		break;
-//
-//        	default:
-//        		Alert alert = new Alert(Alert.AlertType.ERROR, "User type is not valid");
-//        		alert.setHeaderText("Error");
-//        		alert.showAndWait();
-//        	}
-//        }
-//        else {
-//        	Alert alert = new Alert(Alert.AlertType.ERROR, "Failed to log in the "+type);
-//			alert.setHeaderText("Error");
-//			alert.showAndWait();
-//        }
-//    }
+	/**
+     * This method creates a log in request to the server of subscriber, 
+     * and returns the subscriber if log in succeed.
+     *
+     * @param userID ID of the user.
+     * @param password	password of the user.
+     */
+    public Subscriber attemptLoginAsSubscriber(String userID, String password)
+    {
+    	attemptLogin(userID,password,"subscriber");
+    	if(ChatClient.serverResponse)
+    		return ChatClient.subscribers.get(0);
+    	return null;    }
+
+    /**
+     * This method creates a log in request to the server of librarian, 
+     * and returns the librarian if log in succeed.     *
+     * @param userID ID of the user.
+     * @param password	password of the user.
+     */
+    public Librarian attemptLoginAsLibrarian(String userID, String password)
+    {
+    	attemptLogin(userID,password,"librarian");
+    	if(ChatClient.serverResponse)
+    		return ChatClient.librarian;
+    	return null;
+    }
+    
+    /**
+     * This method send a request to the server to log in a user of the given type.
+     *
+     * @param userID ID of the user.
+     * @param password	password of the user.
+     * @param type	type of the user (librarian/subscriber).
+     */
+    private void attemptLogin(String userID, String password, String type){
+    	// Create a list of data of the new user to send to the server
+        ArrayList<String> dataOfLogIn = new ArrayList<>();
+        dataOfLogIn.add(userID);
+        dataOfLogIn.add(password);
+        dataOfLogIn.add(type);
+        ClientUI.chat.accept(new MessageType("100",dataOfLogIn));
+    }
 }
