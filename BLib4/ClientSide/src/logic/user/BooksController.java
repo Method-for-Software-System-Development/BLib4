fd0cc4adef95d79f0568a9289368855d213f153d
Sapir Gerstman <gerstmansapir@gmail.com>
2025-01-21 20:49:59 +0200

    update BooksController

--- BLib4/ClientSide/src/logic/user/BooksController.java
+++ BLib4/ClientSide/src/logic/user/BooksController.java
@@ -3,19 +3,25 @@ package logic.user;
 import java.sql.Connection;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.List;
+
 import entities.logic.MessageType;
 import logic.communication.ChatClient;
 import logic.communication.ClientUI;
 
 
 public class BooksController {
-	private static volatile BooksController instance = null;
+	// Singleton instance of BooksController
+    private static volatile BooksController instance = null;
 
-	/*
-	 * Private constructor for BooksControllers
-	 */
-	public BooksController() {
-	}
+    // List of subscriber IDs with a specific book order
+    public List<String> subscriberIdWithOrder = new ArrayList<>();
+
+    /**
+     * Private constructor for BooksController to implement the Singleton pattern.
+     */
+    private BooksController() {
+    }
 	
 	 /**
      * get the instance of the BooksController for singleton
@@ -38,39 +44,74 @@ public class BooksController {
 	}
 	
 	
+	/**
+     * Gets the list of subscriber IDs associated with an order.
+     *
+     * @return A list of subscriber IDs.
+     */
+    public List<String> getSubscriberIdWithOrder() {
+        return subscriberIdWithOrder;
+    }
+	
+	
 	 /**
      * Checks the availability of a book copy by its ID.
      *
      * @param copyID The ID of the book copy to check.
      * @return true if the book copy is available, false otherwise.
      */
-	public boolean checkAvailability(String copyID) {
+    /**
+     * Checks the availability of a book copy based on its unique ID.
+     * The method communicates with the server to check if the book copy is available.
+     *
+     * @param copyID The unique ID of the book copy to check.
+     * @return 0 if the book copy is available, 
+     *         1 if unavailable with a waitlist,
+     *         2 if the copy id isn't in DB,
+     *        -1 if an error occurs.
+     */
+	public int checkAvailability(String copyID) {
+		if (copyID == null || copyID.isEmpty()) {
+	        System.err.println("Invalid copyID provided");
+	        return -1;
+	    }
 	    try {
+	    	// Clear previous subscriber IDs from the list
+	    	subscriberIdWithOrder.clear();
 	        // Send the copyID to the server to check availability of the copyID
 	        ClientUI.chat.accept(new MessageType("106", copyID));
-
+	        
 	        // Wait for the server's response
-	        if (ChatClient.serverResponse) {
-	            return true;
-	        } else {
+	        // if the copy of book is available
+	        if (ChatClient.availability.get(0).equals("true")) {
+	            return 0;
+	        } else if(ChatClient.availability.size()>1){
+	        	// Add IDs of subscribers who are waiting for the book
+	        	subscriberIdWithOrder.addAll(ChatClient.availability.subList(1, ChatClient.availability.size()));
 	            // If the server indicates the copy is not available
-	            return false;
+	            return 1;
+	        }
+	        else {
+	        	// Book is not in DB
+	        	return 2;
 	        }
 	    } catch (Exception e) {
 	        e.printStackTrace();
-	        return false;
+	        return -1;
 	    }
 	}
 	
-	
 
-	
 	/**
-     * Adds a subscriber to the waitlist for a book if the book is unavailable.
+     * Adds a subscriber to the waitlist for a book that is currently unavailable.
+     * Communicates with the server to perform the action.
      *
-     * @param bookID       The ID of the book to add to the waitlist.
-     * @param subscriberID The ID of the subscriber requesting the book.
-     * @param orderDateTime The date and time the order was placed.
+     * @param bookID        The unique ID of the book to add to the waitlist.
+     * @param subscriberID  The unique ID of the subscriber requesting the book.
+     * @return 1 if the subscriber was successfully added to the waitlist,
+     *         2 if the book has too many reservations,
+     *         3 if the subscriber's account is frozen,
+     *         0 if an unexpected error occurs.
      */
     public int addToWaitlist(String bookID, String subscriberID) {
        try {
@@ -84,21 +125,21 @@ public class BooksController {
         
         // Wait for server response
         // if the book has successfully ordered - return 1
-        if (ChatClient.array.get(0)) {
+        if (ChatClient.availability.get(0).equals("true")) {
+        	// Subscriber successfully added to the waitlist
         	return 1;
         } // if the book cannot be ordered due to a high number of reservations - return 2
-        else if (!ChatClient.array.get(0) && !ChatClient.array.get(1)) {
+        else if (!ChatClient.availability.get(0).equals("true") && !ChatClient.availability.get(1).equals("true")) {
+        	// Too many reservations, cannot add subscriber to waitlist
         	return 2;
         }
-     // if the book cannot be ordered due to a frozen account - return 3
+       // Account is frozen, cannot add subscriber to waitlist
         	return 3;
     } catch (Exception e) {
         e.printStackTrace();
         return 0;
         }
-    }
-    
-    
+    } 
 }
 
 
